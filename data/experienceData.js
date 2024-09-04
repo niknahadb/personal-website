a298cda1c8a754847e7d639a04eb531d174ab881
Borna <niknahad.borna@gmail.com>
2024-09-04 11:22:22 -0700

    update project descrip

--- data/experienceData.js
+++ data/experienceData.js
@@ -3,7 +3,10 @@ const experienceData = [
     name: "Vimbly Group '24",
     title: "Senior Software Engineering Intern @ ",
     time: "May 2024 - Present",
-    description: ["Current Platform development for Mobile Text Alerts"],
+    description: [
+      "Engineered core full-stack features and mobile app screens for Mobile Text Alerts, an online SMS marketing platform that sends 100M+ messages across 20,000+ businesses with over eight figures in annual revenue",
+      "Performed meticulous code reviews, enforcing industry standards to ensure site reliability and scalability",
+    ],
   },
   {
     name: "BillionMinds",
@@ -20,9 +23,19 @@ const experienceData = [
     title: "Software Engineer Intern @ ",
     time: "May 2023 - August 2023",
     description: [
-      "Engineered core frontend functionality and mobile app screens for Mobile Text Alerts, an online SMS marketing platform that sends 100M+ messages across 20,000+ businesses with over eight figures in annual revenue",
-      "Developed a database migration system for MongoDB and wrote unit tests, smoothing developer workflow",
-      "Performed meticulous code reviews, enforcing industry standards to ensure site reliability and scalability",
+      "Developed key features and screens for the mobile app from scratch such as the Settings Screen and Two-Factor Authentication for the mobile app in React Native and Typescript to ensure a smooth, reliable, and responsive user experience leading to an increase of +5,000 weekly visitors",
+      "Enhanced customer interaction by implementing a new AI Text Generator Tool, SmartSMS Suggest, in addition to revamping the customer support platform infrastructure and frontend using React and Node.js to better aid the customer support team which expedited customer complaint resolution times by over 35%",
+      "Documented and resolved Android and iOS codebase issues through streamlined processes and cross-functional communication to deliver a high-quality, error-free application",
+    ],
+  },
+  {
+    name: "Dr.Hell's Lab",
+    title: "Undergraduate Research Assistant @ ",
+    time: "June 2022 - May 2023",
+    description: [
+      "Developed and implemented code to a peptide analysis program via MATLAB, collaborating with a team of developers to improve its functionality and performance by over 150%",
+      "Conducted fluorescence-based protein synthesis on 2 x 10^5 beads on a weekly basis and dissected high-quality membrane tissue protein samples from mice to help the lab reach its goals",
+      "Conducted weekly code reviews to ensure proper syntax quality for readability and functionality",
     ],
   },
 ];
--- data/projectData.js
+++ data/projectData.js
@@ -1,16 +1,23 @@
 const projectData = [
   {
     title: "TrainingTool",
-    description: `DESCRIPTION GOES HERE`,
+    description: `Completely rebuilt CodeLab's internal learning platform, primarily the backend using Java and Spring Boot, to vastly improve the platform's scalability and performance`,
     techStack:
       "Java, Spring Boot, Typescript, React, PostreSQL, Docker, AWS S3, Postman",
     githubLink: "",
   },
   {
     title: "Due Today",
-    description: `DESCRIPTION GOES HERE`,
+    description: `Created a website in React that collects assignment's deadlines from multiple websites, then sorts the data into an interactive calendar to keep track of progress and deadlines with 100% accuracy. 
+    Utilized Axios library along with Express.js and MongoDB to seamlessly fetch and integrate data from Canvas LMS REST API, providing real-time access to students' assignment details on a centralized dashboard.`,
     techStack: "React, Javascript, Express, NodeJS, MongoDB, Mongoose",
     githubLink: "",
+  },
+  {
+    title: "The Plant Paramedic",
+    description: `Automated plant watering system built using C and TI CC3200 Launchpad`,
+    techStack: "C, HTML/CSS",
+    githubLink: "https://niknahadb.github.io/plantparamedic/",
   },
 ];
 
