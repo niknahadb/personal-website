14eee620c671e1b76ca89542afc598536dfbc4fa
Borna <niknahad.borna@gmail.com>
2025-03-25 09:33:43 -0700

    udpate experience

--- data/experienceData.js
+++ data/experienceData.js
@@ -1,21 +1,20 @@
 const experienceData = [
   {
     name: "ASUCD Innovation and Research Lab",
-    title: "Lead Software Developer @ ",
+    title: "Lead Software Engineer @ ",
     time: "Oct 2024 - Present",
     description: [
-      "Lead and manage ucdhacknight.com, where I guide 50+ students in building technically complex personal projects",
+      "Lead and manage ucdhacknight.com, where I guide 50+ students in building and shipping technically complex personal projects",
     ],
   },
   {
     name: "Vimbly Group '24",
-    title: "Senior Software Engineer Intern @ ",
+    title: "Software Engineer Intern @ ",
     time: "May 2024 - August 2024",
     description: [
-      "Engineered core full-stack features and mobile app screens for Mobile Text Alerts, a prominent SMS marketing platform that sends 100M+ messages across 20,000+ businesses with over eight figures in annual revenue",
-      "Built and deployed a scalable Email & SMS verification system for the mobile app in React Native and TypeScript, reducing account fraud by 22% and driving the app's trial-to-paid account conversion rate by 8%",
-      "Elevated backend performance by restructuring key data-handling workflows in Node.js and React alongside optimizing MySQL queries, achieving significant reductions in render time and CPU usage",
-      "Documented and resolved all Android and iOS related issues, collaborating closely with cross-functional teams to deliver a high-quality, error-free application",
+      "Deployed a scalable email & SMS verification system for Mobile Text Alerts in TypeScript and React, reducing account fraud by 28% and driving the app's trial-to-paid account conversion rate by 6% across 20,000+ businesses",
+      "Optimized complex MySQL queries and restructured backend database schemas in Node.js, improving data retrieval efficiency and reducing the average query time for conversations on the Inbox page from 3.5s to less than 400ms",
+      "Documented and resolved all iOS and Android platform related issues, leading debugging efforts and collaborating with cross-functional teams daily to ensure a stable, high-quality mobile application for over 20,000 businesses",
     ],
   },
   {
@@ -23,9 +22,8 @@ const experienceData = [
     title: "Software Engineer Intern @ ",
     time: "Sep 2023 - Dec 2023",
     description: [
-      "Built two comprehensive web dashboards for BillionMinds to showcase real-time adaptibility and resilience metrics using Next.js, MongoDB, and Express for optimized performance and data managemen",
-      "Led the development and refinement of multiple Next.js prototypes into a cohesive production-ready product, nearly cutting the development cycle in half through efficient project management and iterative feedback with the client",
-      "Implemented data aggregation with AWS Lambda serverless functions, reducing data processing latency by 40% and enhancing dashboard responsiveness",
+      "Designed and deployed several comprehensive web dashboards to showcase real-time adaptibility and resilience metrics using Next.js, MongoDB, and Express.js for optimized performance and data management",
+      "Implemented data aggregation with AWS Lambda serverless functions, cutting data processing latency in half and enhancing dashboard responsiveness",
     ],
   },
   {
@@ -33,8 +31,9 @@ const experienceData = [
     title: "Software Engineer Intern @ ",
     time: "May 2023 - August 2023",
     description: [
-      "Developed key mobile app features and screens for Mobile Text Alerts such as multi-factor authentication and the Settings Screen in React Native and Typescript to deliver a reliable and responsive application, leading to an increase of +5,000 weekly visitors",
-      "Redesigned and optimized the admin platform's infrastructure and frontend with React and Node.js, resulting in nearly a 50% reduction in complaint resolution times and a significant boost in customer support efficiency",
+      "Drove 5,000+ new weekly users by leading end-to-end development of core mobile app features in TypeScript and React Native, including a redesigned Settings screen and seamless onboarding experience for new users on the app",
+      "Revamped and upgraded the admin platform's infrastructure and frontend using React and Node.js, saving the customer support team and the company over 120 hours per month in complaint resolution time",
+      "Led QA efforts by owning the testing life cycle, from writing unit tests to CI/CD pipeline validation across platforms",
       "Maintained a focus on code quality, performance optimization, and adherence to industry standards throughout the development process",
     ],
   },
